c1c03c655256b5b5f114d599f9221d6caaaed1b6
oluoko <brianotieno586@gmail.com>
2025-01-22 04:07:43 +0300

    Skilluminati: Assets for OnBoarding Screen

--- screens/onboarding/onboarding.screen.tsx
+++ screens/onboarding/onboarding.screen.tsx
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { onBoardingSlides } from "@/configs/constants";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function OnboardingScreen() {
-  return (
-    <View>
-      <Text>OnboardingScreen</Text>
-    </View>
-  );
+  const [index, setIndex] = useState(0);
+  const prev = onBoardingSlides[index - 1];
+  const next = onBoardingSlides[index + 1];
+
+  return <GestureHandlerRootView className=""></GestureHandlerRootView>;
 }
 
 const styles = StyleSheet.create({});
